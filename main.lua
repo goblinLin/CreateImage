84ad977d860dc16d9f293d23daf8efcd776ae30a
Pin ChuehLin <zack@noahgames.com>
2017-02-13 11:17:08 +0800

    內容追加

--- main.lua
+++ main.lua
@@ -14,28 +14,40 @@ SCREEN = {
 	WIDTH = display.viewableContentWidth,
 	HEIGHT = display.viewableContentHeight
 }
-
+print( 'SCREEN WIDTH:' , SCREEN.WIDTH )
 SCREEN.CENTER = {
-	x = display.contentCenterX,
-	y = display.contentCenterY
+	X = display.contentCenterX,
+	Y = display.contentCenterY
 }
---顯示一張圖片，參數分別為路徑,寬度,高度
-local image1 = display.newImageRect(  "images/magic.jpg", 300, 200 )
 
---設定圖片的錨點
-image1.anchorx = image1.width / 2
-image1.anchory = image1.height / 2
+--display.setStatusBar( display.DefaultStatusBar )
+display.setStatusBar( display.TranslucentStatusBar )
 
---設定圖片的座標
-image1.x = SCREEN.CENTER.x
-image1.y = SCREEN.CENTER.y
+local init
 
---設定圖片的透明度
-image1.alpha = 0.5
+init = function (  )
+	--顯示一張圖片，參數分別為路徑,寬度,高度
+	local image1 = display.newImageRect(  "images/magic.jpg", 300, 200 )
+	--設定圖片的錨點
+	image1.anchorX , image1.anchorY = 0 , 0
 
---設定圖片的縮放比例
-image1.xScale = 0.5
-image1.yScale = 2
+	--設定圖片的座標
+	image1.x = SCREEN.CENTER.X
+	image1.y = SCREEN.CENTER.Y
 
---設定圖片的旋轉度
-image1.rotation = 45
+	--設定圖片的透明度
+	image1.alpha = 0.5
+
+	--設定圖片的縮放比例
+	image1.xScale = 0.5
+	image1.yScale = 2
+
+	--設定圖片的旋轉度
+	--image1.rotation = 45
+
+	image1.isVisible = false
+end
+
+init()
+
+
